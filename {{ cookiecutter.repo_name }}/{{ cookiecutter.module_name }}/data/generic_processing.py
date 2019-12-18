7c1cf54d982477e967d7a6ece0744e37c0f870f9
waveFrontSet <paul.bubenzer@gmail.com>
2019-12-18 19:04:01 +0100

    Extracted logging config file
    - Added logs folder
    - Added dictConfig that defines a debug and warning level
    RotatingFileHandler as well as the usual StreamHandler that logs to console

--- {{ cookiecutter.repo_name }}/{{ cookiecutter.module_name }}/data/generic_processing.py
+++ {{ cookiecutter.repo_name }}/{{ cookiecutter.module_name }}/data/generic_processing.py
@@ -1,8 +1,12 @@
 # -*- coding: utf-8 -*-
 import click
 import logging
-from pathlib import Path
-from dotenv import find_dotenv, load_dotenv
+import logging.config
+from ..log_config import LOGGING
+
+
+logging.config.dictConfig(LOGGING)
+logger = logging.getLogger(__name__)
 
 
 @click.command()
@@ -19,19 +23,8 @@ def main(input_filepath, output_filepath):
     - Remove columns that only contain technical information and are not
     needed for aggregation (ids)
     """
-    logger = logging.getLogger(__name__)
     logger.info("Running generic data processing and cleanup scripts...")
 
 
 if __name__ == "__main__":
-    log_fmt = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
-    logging.basicConfig(level=logging.INFO, format=log_fmt)
-
-    # not used in this stub but often useful for finding various files
-    project_dir = Path(__file__).resolve().parents[2]
-
-    # find .env automagically by walking up directories until it's found, then
-    # load up the .env entries as environment variables
-    load_dotenv(find_dotenv())
-
     main()
--- {{ cookiecutter.repo_name }}/{{ cookiecutter.module_name }}/features/build_features.py
+++ {{ cookiecutter.repo_name }}/{{ cookiecutter.module_name }}/features/build_features.py
@@ -1,8 +1,12 @@
 # -*- coding: utf-8 -*-
 import click
 import logging
-from pathlib import Path
-from dotenv import find_dotenv, load_dotenv
+import logging.config
+from ..log_config import LOGGING
+
+
+logging.config.dictConfig(LOGGING)
+logger = logging.getLogger(__name__)
 
 
 @click.command()
@@ -17,19 +21,8 @@ def main(input_filepath, output_file):
     - Extract columns from different files in interim data
     - Aggregate over grouped data
     """
-    logger = logging.getLogger(__name__)
     logger.info("Making final data set from interim data...")
 
 
 if __name__ == "__main__":
-    log_fmt = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
-    logging.basicConfig(level=logging.INFO, format=log_fmt)
-
-    # not used in this stub but often useful for finding various files
-    project_dir = Path(__file__).resolve().parents[2]
-
-    # find .env automagically by walking up directories until it's found, then
-    # load up the .env entries as environment variables
-    load_dotenv(find_dotenv())
-
     main()
