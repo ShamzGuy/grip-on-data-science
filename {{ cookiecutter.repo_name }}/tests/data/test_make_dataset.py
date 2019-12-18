30e9486d35dae5e9568f6dd0d6ca541ea2b7c18a
waveFrontSet <paul.bubenzer@gmail.com>
2019-12-18 15:21:37 +0100

    Black'ed the generated repository.

--- {{ cookiecutter.repo_name }}/tests/data/test_make_dataset.py
+++ {{ cookiecutter.repo_name }}/tests/data/test_make_dataset.py
@@ -4,7 +4,7 @@ from {{ cookiecutter.module_name }}.data.make_dataset import main
 def test_main():
     runner = CliRunner()
     with runner.isolated_filesystem():
-        with open('hello.txt', 'w') as f:
-            f.write('Hello World!')
-        result = runner.invoke(main, ['hello.txt', 'output_path'])
+        with open("hello.txt", "w") as f:
+            f.write("Hello World!")
+        result = runner.invoke(main, ["hello.txt", "output_path"])
         assert result.exit_code == 0
